c255a0df4523c1a25cb12ba89cc7e3ff1ee5d8b4
Serey Roth <sereyratanakroth@gmail.com>
2023-02-15 12:02:38 -0800

    Create service class for accessing link resources in database

---
+++ api/src/main/java/com/linktreeclone/api/service/LinkService.java
@@ -0,0 +1,42 @@
+package com.linktreeclone.api.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import com.linktreeclone.api.dao.LinkDao;
+import com.linktreeclone.api.model.Link;
+
+@Service
+public class LinkService {
+
+    @Autowired
+    private final LinkDao linkDao;
+
+    public LinkService(@Qualifier("postgres-link") LinkDao linkDao) {
+        this.linkDao = linkDao;
+    }
+
+    public boolean addLink(Link link) {
+        return linkDao.addLink(link);
+    };
+
+    public Optional<Link> selectLinkById(Long id) {
+        return linkDao.selectLinkById(id);
+    }
+    
+    public List<Link> selectAllLinksByCreatorId(Long creatorId) {
+        return linkDao.selectAllLinksByCreatorId(creatorId);
+    }
+    
+    public boolean deleteLinkById(Long id) {
+        return linkDao.deleteLinkById(id);
+    }
+
+    public Optional<Link> updateLinkById(Long id, Link newLink) {
+        return linkDao.updateLinkById(id, newLink);
+    }
+}
