2cb1ca7d31b65b6bc025c361e6538e5ee7bc7047
Serey Roth <sereyratanakroth@gmail.com>
2023-02-15 11:16:00 -0800

    Create POJO for authentication request object with username and password

---
+++ api/src/main/java/com/linktreeclone/api/payload/request/AuthRequest.java
@@ -0,0 +1,42 @@
+package com.linktreeclone.api.payload.request;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class AuthRequest {
+    @NotBlank(message = "Username is mandatory!")
+    private String username;
+
+    @NotBlank(message = "Password is mandatory!")
+    @Size(min = 6)
+    private String password;
+
+    public AuthRequest() {}
+
+    public AuthRequest(
+        @JsonProperty("username") String username,
+        @JsonProperty("password") String password
+    ) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
