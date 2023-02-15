60c65930a65881dab193eec35032105895cea3ed
Serey Roth <sereyratanakroth@gmail.com>
2023-02-15 11:26:44 -0800

    Create repository for Link entity

---
+++ api/src/main/java/com/linktreeclone/api/repository/LinkRepository.java
@@ -0,0 +1,16 @@
+package com.linktreeclone.api.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.linktreeclone.api.model.Link;
+
+import jakarta.transaction.Transactional;
+
+public interface LinkRepository extends JpaRepository<Link, Long>  {
+    List<Link> findByCreatorId(Long creatorId);
+
+    @Transactional
+    void deleteByCreatorId(Long creatorId);
+}
