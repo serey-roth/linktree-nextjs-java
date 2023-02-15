eff02803a58d2dbb1d3e6b3fe00006bf32e39ea5
Serey Roth <sereyratanakroth@gmail.com>
2023-02-15 11:06:00 -0800

    Add roles with many-to-many relationship to User entity and add annotations for field validations

--- api/src/main/java/com/linktreeclone/api/model/User.java
+++ api/src/main/java/com/linktreeclone/api/model/User.java
@@ -1,13 +1,26 @@
 package com.linktreeclone.api.model;
 
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.validator.constraints.Length;
+
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
 @JsonIgnoreProperties({"hibernateLazyInitializer"})
@@ -19,21 +32,43 @@ public class User {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @Column(name = "username")
     @NotBlank(message = "Username is mandatory!")
+    @Column(name = "username", unique = true)
     private String username;
 
-    @Column(name = "email")
     @NotBlank(message = "Email is mandatory!")
+    @Email(message = "Please enter a valid email address!")
+    @Column(name = "email", unique = true)
     private String email;
 
-    @Column(name = "password")
     @NotBlank(message = "Password is mandatory!")
+    @Length(min = 6, message = "Password should be at least 6 characters long!")
+    @Column(name = "password")
     private String password;
+
+    @CreationTimestamp
+    @Column(name = "createdAt", updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updatedAt")
+    private LocalDateTime updatedAt;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+        name = "user_roles",
+        joinColumns = @JoinColumn(name = "user_id"),
+        inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles = new HashSet<>();
 
     public User() {}
 
-    public User(String username, String email, String password) {
+    public User(
+        String username,
+        String email,
+        String password
+    ) {
         this.username = username;
         this.email = email;
         this.password = password;
@@ -51,6 +86,14 @@ public class User {
         this.password = password;
     }
 
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
     public String getUsername() {
         return this.username;
     }
@@ -61,5 +104,9 @@ public class User {
 
     public String getPassword() {
         return this.password;
+    }
+    
+    public Set<Role> getRoles() {
+        return this.roles;
     }
 }
