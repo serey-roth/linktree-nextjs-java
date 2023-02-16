3e821a065ff5bb34832b236394cb47acc32059f0
Serey Roth <sereyratanakroth@gmail.com>
2023-02-15 19:57:03 -0800

    Update AuthEntryPointJwt commence function to handle authentication exception and display custom error response

--- api/src/main/java/com/linktreeclone/api/security/jwt/AuthEntryPointJwt.java
+++ api/src/main/java/com/linktreeclone/api/security/jwt/AuthEntryPointJwt.java
@@ -4,23 +4,46 @@ import java.io.IOException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.linktreeclone.api.exception.ApiErrorResponse;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@Component
+@Component("authEntryPointJwt")
 public class AuthEntryPointJwt implements AuthenticationEntryPoint {
 
     private static final Logger logger = LoggerFactory.getLogger(AuthEntryPointJwt.class);
+    
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response,
         AuthenticationException authException) throws IOException, ServletException {
         logger.error("Unauthorized error: {}", authException.getMessage());
-        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Error: Unauthorized");
+        try {
+            ApiErrorResponse errorResponse = new ApiErrorResponse(
+                HttpStatus.UNAUTHORIZED,
+                "401",
+                "User is unauthenticated!",
+                "Full authentication is required to access resources"
+            );
+            response.setStatus(HttpStatus.UNAUTHORIZED.value());
+            response.setContentType("application/json");
+            response.setCharacterEncoding("UTF-8");
+            ObjectMapper mapper = new ObjectMapper();
+            mapper.registerModule(new JavaTimeModule());
+            mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, true);
+            mapper.writeValue(response.getOutputStream(), errorResponse);
+        } catch (Exception ex) {
+            throw ex;
+        }
     }
     
 }
