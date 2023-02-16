ba5e3f9d5c1145a3cb9f65ac7b052e9acb216c6c
Serey Roth <sereyratanakroth@gmail.com>
2023-02-15 22:31:16 -0800

    Create global handler for LinkNotFoundException

--- api/src/main/java/com/linktreeclone/api/exception/GlobalExceptionHandler.java
+++ api/src/main/java/com/linktreeclone/api/exception/GlobalExceptionHandler.java
@@ -1,9 +1,35 @@
 package com.linktreeclone.api.exception;
 
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+@Order(Ordered.HIGHEST_PRECEDENCE)
 @ControllerAdvice
+@RestController
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     
+    @ExceptionHandler(LinkNotFoundException.class)
+    public ResponseEntity<ApiErrorResponse> handleLinkNotFoundException(
+        LinkNotFoundException e,
+        WebRequest request
+    ) throws Exception {
+        try {
+            ApiErrorResponse errorResponse = new ApiErrorResponse(
+                HttpStatus.NOT_FOUND,
+                "404",
+                e.getMessage(),
+                e.getDetails()
+            );
+            return new ResponseEntity<ApiErrorResponse>(errorResponse, HttpStatus.NOT_FOUND);
+        } catch (Exception ex) {
+            throw ex;
+        }
+    }
 }
