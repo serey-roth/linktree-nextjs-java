08c159503b984efde9cb97841f805b8b36626791
Serey Roth <sereyratanakroth@gmail.com>
2023-02-25 18:30:13 -0800

    Add handler for MissingRequestHeaderException

--- api/src/main/java/com/linktreeclone/api/exception/GlobalExceptionHandler.java
+++ api/src/main/java/com/linktreeclone/api/exception/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MissingRequestHeaderException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
@@ -52,6 +53,29 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
                 "400",
                 e.getMessage(),
                 e.getDetails()
+            );
+            return new ResponseEntity<ApiResponse<JwtResponse>>(
+                new ApiResponse<JwtResponse>(
+                    null, 
+                    errorResponse
+                ), HttpStatus.OK
+            );
+        } catch (Exception ex) {
+            throw ex;
+        }
+    }
+
+    @ExceptionHandler(MissingRequestHeaderException.class)
+    public ResponseEntity<ApiResponse<JwtResponse>> handleMissingRequestHeaderException(
+        MissingRequestHeaderException e,
+        WebRequest request
+    ) throws Exception {
+        try {
+            ApiErrorResponse errorResponse = new ApiErrorResponse(
+                HttpStatus.BAD_REQUEST,
+                "400",
+                e.getMessage(),
+                e.getBody().getDetail()
             );
             return new ResponseEntity<ApiResponse<JwtResponse>>(
                 new ApiResponse<JwtResponse>(
