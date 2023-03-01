fcd9248da6fd0c55b3565ede13144acd4a980d98
Serey Roth <sereyratanakroth@gmail.com>
2023-02-28 16:47:37 -0800

    Update paginated and paginated-sorted options for selectLinksByCreatedId inside LinkDaoPostgresImpl to return Page instead of List

--- api/src/main/java/com/linktreeclone/api/dao/LinkDaoPostgresImpl.java
+++ api/src/main/java/com/linktreeclone/api/dao/LinkDaoPostgresImpl.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
@@ -61,7 +62,7 @@ public class LinkDaoPostgresImpl implements LinkDao {
     }
 
     @Override
-    public List<Link> selectPaginatedLinksByCreatorId(
+    public Page<Link> selectPaginatedLinksByCreatorId(
         Long creatorId, 
         int pageCount, 
         int pageNumber
@@ -71,7 +72,7 @@ public class LinkDaoPostgresImpl implements LinkDao {
     }
 
     @Override
-    public List<Link> selectPaginatedSortedLinksByCreatorId(
+    public Page<Link> selectPaginatedSortedLinksByCreatorId(
         Long creatorId, 
         int pageCount, 
         int pageNumber,
