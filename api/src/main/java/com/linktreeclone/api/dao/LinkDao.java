b145e122c325a3b6f115e7a6d649c35d79385262
Serey Roth <sereyratanakroth@gmail.com>
2023-03-18 19:35:14 -0700

    Add selectAllLinks method to LinkDao and implements IdentifiedPaginationDao inside LinkDao

--- api/src/main/java/com/linktreeclone/api/dao/LinkDao.java
+++ api/src/main/java/com/linktreeclone/api/dao/LinkDao.java
@@ -1,10 +1,13 @@
 package com.linktreeclone.api.dao;
 
+import java.util.List;
 import java.util.Optional;
 
 import com.linktreeclone.api.model.Link;
 
-public interface LinkDao extends Dao<Link> {
+public interface LinkDao extends IdentifiedPaginationDao<Link> {
+    
+    List<Link> selectAllLinks();
     
     boolean addLink(Link link);
 
@@ -13,4 +16,7 @@ public interface LinkDao extends Dao<Link> {
     boolean deleteLinkById(Long id);
 
     Optional<Link> updateLinkById(Long id, Link newLink);
+
+    List<Link> selectAllLinksByCreatorId(Long creatorId);
+
 }
